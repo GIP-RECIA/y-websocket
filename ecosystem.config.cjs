6750a076ef202ba09d81c3b0d181af1df47fb085
Quentin Guillemin <quentin.guillemin@recia.fr>
2025-04-09 12:35:33 +0200

    build: bump node version from 20 to 22

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,18 +1,24 @@
 module.exports = {
   apps : [{
-    name: "y-websocket",
-    script: "./bin/server.cjs",
-    interpreter: "node@20.13.1",
+    name: 'y-websocket',
+    script: './bin/server.cjs',
+    // interpreter: 'node@22.14.0',
+    // exec_mode: 'cluster',
+    // instances: 4,
+    max_memory_restart: '500M',
     env: {
-      HOST: "0.0.0.0",
+      HOST: '0.0.0.0',
       PORT: 1234,
       REDIS: false,
-      REDIS_HOST: '',
+      REDIS_HOST: '0.0.0.0',
       REDIS_PORT: 6379,
-      REDIS_PREFIX: '',
+      REDIS_PREFIX: 'YWS_',
       REDIS_PATH: '',
       REDIS_PASSWORD: undefined
     },
-    cron_restart: "0 0 * * *",
+    combine_logs: true,
+    merge_logs: true,
+    time: true,
+    cron_restart: '0 0 * * *',
   }]
 }
